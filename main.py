523f3890a9d51693913a4fa4d7e5fd9e1df4b290
Spencer Miskoviak <spencer@skovistudios.com>
2016-03-09 19:12:22 -0800

    added dendro heatmap, fix input

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.hierarchical import Dendrogram
+from src.hierarchical import Dendrogram, DendrogramHeatmap
 from src.kmeans import KMeans
 from src.data import Data
 import sys
@@ -6,14 +6,17 @@ import sys
 limit_total = 100 # default to 100 samples
 k = 10 # default to 10 clusters
 if len(sys.argv) > 1:
-    limit_total = sys.argv[1]
+    limit_total = int(sys.argv[1])
 if len(sys.argv) > 2:
-    k = sys.argv[2]
-
-print 'Creating a Dendrogram with ' + str(limit_total) + ' samples...'
-d = Dendrogram(limit_total)
+    k = int(sys.argv[2])
 
 print 'Clustering with ' + str(limit_total) + ' samples and ' + str(k) + ' clusters...'
 k = KMeans(Data(limit_total), k)
 k.perform_clustering()
 k.print_clusters()
+
+print 'Creating a Dendrogram with ' + str(limit_total) + ' samples...'
+d = Dendrogram(limit_total)
+
+print 'Creating a Dendrogram Heatmap with ' + str(limit_total) + ' samples...'
+dh = DendrogramHeatmap(limit_total)
--- src/hierarchical.py
+++ src/hierarchical.py
@@ -1,7 +1,10 @@
 import numpy as np
 import plotly
 from data import Data
-from plotly.tools import FigureFactory
+import plotly.plotly as py
+import plotly.graph_objs as go
+from plotly.tools import FigureFactory as FF
+from scipy.spatial.distance import pdist, squareform
 
 class Dendrogram:
     def __init__(self, limit_total):
@@ -21,9 +24,107 @@ class Dendrogram:
     # create a dendrogram from the data and generate a HTML file
     def generate_dendrogram(self):
         x = np.array(self.data.distance_matrix)
-        fig = FigureFactory.create_dendrogram(x, orientation='left', labels=self.names)
+        fig = FF.create_dendrogram(x, orientation='left', labels=self.names)
         fig['layout'].update({'width': 1600, 'height': self.dendrogram_height(), 'margin': { 'l': 400 }})
         plotly.offline.plot(fig, filename='output/dendrogram.html')
 
     def dendrogram_height(self):
         return 40 * self.limit_total
+
+class DendrogramHeatmap:
+    def __init__(self, limit_total):
+        self.data = Data(limit_total) # data source and basic computations
+        self.limit_total = limit_total # number of samples
+        self.names = [] # names of samples in order
+
+        # populate data and create dendrogram
+        self.generate_names()
+        self.generate_dendrogram()
+
+    # create the list of labels to apply to each sample
+    def generate_names(self):
+        for row in self.data.data:
+            self.names.append(row[0])
+
+    # create a dendrogram from the data and generate a HTML file
+    def generate_dendrogram(self):
+        labels = self.names
+        x = np.array(self.data.distance_matrix)
+
+        # Initialize figure by creating upper dendrogram
+        figure = FF.create_dendrogram(x, orientation='bottom', labels=labels)
+        for i in range(len(figure['data'])):
+            figure['data'][i]['yaxis'] = 'y2'
+
+        # Create Side Dendrogram
+        dendro_side = FF.create_dendrogram(x, orientation='right')
+        for i in range(len(dendro_side['data'])):
+            dendro_side['data'][i]['xaxis'] = 'x2'
+
+        # Add Side Dendrogram Data to Figure
+        figure['data'].extend(dendro_side['data'])
+
+        # Create Heatmap
+        dendro_leaves = dendro_side['layout']['yaxis']['ticktext']
+        dendro_leaves = list(map(int, dendro_leaves))
+        data_dist = pdist(x)
+        heat_data = squareform(data_dist)
+        heat_data = heat_data[dendro_leaves,:]
+        heat_data = heat_data[:,dendro_leaves]
+
+        heatmap = go.Data([
+            go.Heatmap(
+                x = dendro_leaves,
+                y = dendro_leaves,
+                z = heat_data,
+                colorscale = 'YIGnBu'
+            )
+        ])
+
+        heatmap[0]['x'] = figure['layout']['xaxis']['tickvals']
+        heatmap[0]['y'] = dendro_side['layout']['yaxis']['tickvals']
+
+        # Add Heatmap Data to Figure
+        figure['data'].extend(go.Data(heatmap))
+
+        # Edit Layout
+        figure['layout'].update({'width':1600, 'height':1600,
+                                 'showlegend':False, 'hovermode': 'closest',
+                                 'margin': { 'b': 400 }
+                                 })
+
+        # Edit xaxis
+        figure['layout']['xaxis'].update({'domain': [.15, 1],
+                                          'mirror': False,
+                                          'showgrid': False,
+                                          'showline': False,
+                                          'zeroline': False,
+                                          'ticks':""})
+        # Edit xaxis2
+        figure['layout'].update({'xaxis2': {'domain': [0, .15],
+                                           'mirror': False,
+                                           'showgrid': False,
+                                           'showline': False,
+                                           'zeroline': False,
+                                           'showticklabels': False,
+                                           'ticks':""}})
+
+        # Edit yaxis
+        figure['layout']['yaxis'].update({'domain': [0, .85],
+                                          'mirror': False,
+                                          'showgrid': False,
+                                          'showline': False,
+                                          'zeroline': False,
+                                          'showticklabels': False,
+                                          'ticks': ""})
+        # Edit yaxis2
+        figure['layout'].update({'yaxis2':{'domain':[.825, .975],
+                                           'mirror': False,
+                                           'showgrid': False,
+                                           'showline': False,
+                                           'zeroline': False,
+                                           'showticklabels': False,
+                                           'ticks':""}})
+
+        # Plot!
+        plotly.offline.plot(figure, filename='output/dendrogram_with_heatmap.html')
